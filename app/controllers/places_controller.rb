64671128cb30f9a032084fe9002e5cdfe393f9ce
vigru3 <vigru3@protonmail.com>
2020-03-22 20:07:14 -0700

    before devise

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,19 @@ class PlacesController < ApplicationController
   def index
     @pagy, @places = pagy(Place.all)
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
  root 'places#index' 
+
+ resources :places
 end
