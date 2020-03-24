fc33086179c2e161c03c1e28bba4b02e2ad84f41
vigru3 <vigru3@protonmail.com>
2020-03-23 23:31:41 -0700

    comment modal

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @pagy, @comments = pagy(Comment.all)
   end
 
   def edit
